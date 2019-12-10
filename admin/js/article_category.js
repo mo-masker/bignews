2068e60d75f97bbbe9ad6c9c5ab96e1c6a142134
mo-masker <1050251827@qq.com>
2019-12-10 19:53:26 +0800

    文章类别管理-实现编辑功能

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -1,32 +1,77 @@
-$(function(){
+$(function () {
     // 先获取所有文章分类渲染在页面上
     $.ajax({
-        url:BigNew.category_list,
-        type:'get',
-        success:function(res){
+        url: BigNew.category_list,
+        type: 'get',
+        success: function (res) {
             // console.log(res);
-            let htmlStr = template('categoryList',res);
+            let htmlStr = template('categoryList', res);
             $('tbody').html(htmlStr)
         }
     })
 
 
+    // 当点击取消按钮的时候，将表单中的数据全部重置
+    $('#btn-cancel').on('click',function(){
+        $('form'[0].reset());
+    })
+
+
     // 当模态框在显示的时候，获取当前点击的按钮
-    $('#myModal').on('show.bs.modal',function(e){
+    $('#myModal').on('show.bs.modal', function (e) {
         // e.relatedTarget 能获取到触发这个模态框显示的dom元素
         let dom = e.relatedTarget;
-        console.log(dom);
+        // console.log(dom);
 
         // 判断到底是谁触发了这个模态框显示
-        if(dom == $('#xinzengfenlei')[0]){
+        if (dom == $('#xinzengfenlei')[0]) {
             // console.log($('#xinzengfenlei'));
             // console.log($('#xinzengfenlei')[0]);
             // 改变模态框的文本
             $('#exampleModalLabel').text('新增文章分类');
             $('#btn-confirm').text('新增').addClass('btn-success').removeClass('btn-primary');
-        }else{
+        } else {
             $('#exampleModalLabel').text('编辑文章分类');
             $('#btn-confirm').text('编辑').addClass('btn-primary').removeClass('btn-success');
+            //当弹出编辑的模态框的时候，需要得到一个文章类别id，根据这个id，发送ajax请求获得具体的文章类别信息
+            let cateId = $(dom).attr('data-id');
+            // console.log(cateId);
+            $.get({
+                url: BigNew.category_search,
+                data: {
+                    id: cateId,
+                },
+                success: function (res) { 
+                    // console.log(res);
+                    if(res.code == 200){
+                        $('#recipient-name').val(res.data[0].name);
+                        $('#message-text').val(res.data[0].slug);
+                        $('#cateid').val(res.data[0].id);
+                    }
+                }
+            })
+
+            // 模态框的编辑按钮 收集更改后的数据，发送给服务器
+            $('#btn-confirm').on('click',function(){
+                let name = $('#recipient-name').val();
+                let slug = $('#message-text').val();
+                let id = $('#cateid').val();
+                $.post({
+                    url:BigNew.category_edit,
+                    data:{
+                        id:id,
+                        name:name,
+                        slug:slug
+                    },
+                    success:function(res){
+                        // console.log(res);
+                        // 修改成功隐藏模态框
+                        if(res.code == 200){
+                            $('#myModal').modal('hide');
+                        }
+                    }
+                })
+            })
         }
     })
 })
