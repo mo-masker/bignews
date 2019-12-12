d3db58375b4d5be31657204e720ea0a4de317e90
mo-masker <1050251827@qq.com>
2019-12-12 18:26:14 +0800

    文章列表-实现获取文章数据、筛选功能、分页跳转

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -4,7 +4,7 @@ $(function () {
     $.get({
         url: BigNew.category_list,
         success: function (res) {
-            console.log(res);
+            // console.log(res);
             if (res.code == 200) {
                 //获取成功把类名数据写入模板
                 let htmlStr = template('categoryList', res);
@@ -13,10 +13,77 @@ $(function () {
         }
     })
 
-    // 定义两个变量，用来表示页码和条数
-    let mypage = 1;  // 默认第一页
-    let perpage = 10;  // 默认10条数据
-    // 页面一加载就展示文章条目数据
-    
-    
+    // 获取所有文章信息
+    $.get({
+        url: BigNew.article_query,
+        data: {
+            type: $('#selCategory').val(),
+            state: $('#selStatus').val(),
+            page: 1,
+            perpage: 10
+        },
+        success: function (res) {
+            // console.log(res);
+            // 获取成功写入模板
+            let htmlStr = template('artList', res);
+            $('tbody').html(htmlStr);
+
+
+            //将页面的页码条渲染出来 分页
+            $('#pagination-demo').twbsPagination({
+                totalPages: res.data.totalPage,
+                visiblePages: 7,
+                first: '首页',
+                prev: '上一页',
+                next: '下一页',
+                last: '尾页',
+                // 页码的点击事件，发送请求获取数据
+                onPageClick: function (event, page) {
+                    $.get({
+                        url: BigNew.article_query,
+                        data: {
+                            type: $('#selCategory').val(),
+                            state: $('#selStatus').val(),
+                            page: page,
+                            perpage: 10
+                        },
+                        success: function (res) {
+                            // 获取成功写入模板
+                            let htmlStr = template('artList', res);
+                            $('tbody').html(htmlStr);
+                        }
+                    })
+                }
+            })
+        }
+    });
+
+
+
+    //筛选按钮注册点击事件
+    $('#btnSearch').on('click', function (e) {
+        //阻止默认行为
+        e.preventDefault();
+
+        $.get({
+            url: BigNew.article_query,
+            data: {
+                type: $('#selCategory').val(),
+                state: $('#selStatus').val(),
+                page: 1,
+                perpage: 10
+            },
+            success: function (res) {
+                console.log(res);
+                // 搜索成功把数据写入模板
+                let htmlStr = template('artList', res);
+                $('tbody').html(htmlStr);
+                //搜索到的数据条目是不一样的，所以总的页数是会动态改变的
+                $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, 1)
+            }
+        });
+    })
+
+
+
 })
