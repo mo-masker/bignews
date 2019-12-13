66913b1a914d3cb798539fa29877f414d5093134
mo-masker <1050251827@qq.com>
2019-12-13 10:57:02 +0800

    实现修改文章功能

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -35,7 +35,7 @@ $(function () {
     // 根据文章id获取到文章的具体数据，然后渲染到页面表单中
     // 获取文章id
     let articleId = location.search.split('=')[1];
-    console.log(articleId);
+    // console.log(articleId);
     //发送ajax请求
     $.get({
         url:BigNew.article_search,
@@ -52,6 +52,36 @@ $(function () {
                 editor.txt.html(res.data.content);
             }
         }
+    })
+
+    // 修改文章功能
+    // 当点击修改按钮的时候，发送请求将文章数据更新，注册点击事件
+    $('button.btn-edit').on('click',function(e){
+        // 阻止默认行为
+        e.preventDefault();
+        // 使用FormData方式来提交数据
+        let fd = new FormData($('#form')[0]);
+        fd.append('content',editor.txt.html());
+        // 修改完文章，默认是已经发布
+        fd.append('id',articleId);
+
+        // 发送ajax请求
+        $.post({
+            url:BigNew.article_edit,
+            data:fd,
+            // 不处理数据
+            processData:false,
+            // 不转换类型
+            contentType:false,
+            success:function(res){
+                // console.log(res);
+                if(res.code == 200){
+                    // 回到上一个页面
+                    window.history.back();
+                }
+            }
+
+        })
     })
 
 })
