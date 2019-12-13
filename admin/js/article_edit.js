08981bb8b548254dc02a81fef5679707bd96d821
mo-masker <1050251827@qq.com>
2019-12-13 10:10:15 +0800

    获取文章数据-渲染到编辑页面

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -32,6 +32,26 @@ $(function () {
     editor.create()
 
 
-    
+    // 根据文章id获取到文章的具体数据，然后渲染到页面表单中
+    // 获取文章id
+    let articleId = location.search.split('=')[1];
+    console.log(articleId);
+    //发送ajax请求
+    $.get({
+        url:BigNew.article_search,
+        data:{
+            id:articleId
+        },
+        success:function(res){
+            // console.log(res);
+            if(res.code == 200){
+                $('#inputTitle').val(res.data.title);
+                $('img.article_cover').attr('src',res.data.cover);
+                $('select.category').val(res.data.categoryId);
+                $('#testico').val(res.data.date);
+                editor.txt.html(res.data.content);
+            }
+        }
+    })
 
 })
