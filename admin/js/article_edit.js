ff1e5ac6537c594f4a83ee0ed23327323ff7762b
mo-masker <1050251827@qq.com>
2019-12-13 09:13:31 +0800

    添加富文本编辑器

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -1,18 +1,18 @@
-$(function(){
+$(function () {
     // 1.实现图片预览效果
-    $('#inputCover').on('change',function(){
+    $('#inputCover').on('change', function () {
         // 转化为文件流
         let imgFile = this.files[0];
         let url = URL.createObjectURL(imgFile);
-        $('.article_cover').attr('src',url);
+        $('.article_cover').attr('src', url);
     })
 
     // 2.让文章分类加载完成
     $.get({
-        url:BigNew.category_list,
-        success:function(res){
+        url: BigNew.category_list,
+        success: function (res) {
             // console.log(res);
-            let htmlStr = template('categoryList',res);
+            let htmlStr = template('categoryList', res);
             $('select.category').html(htmlStr)
         }
     })
@@ -22,7 +22,13 @@ $(function(){
         format: "YYYY-MM-DD",
         isTime: false,
         minDate: "2014-09-19 00:00:00",
-        zIndex:30000
+        zIndex: 30000
     })
+
+    // 4.添加富文本编辑器
+    var E = window.wangEditor
+    var editor = new E('#editor')
+    // 或者 var editor = new E( document.getElementById('editor') )
+    editor.create()
 
 })
